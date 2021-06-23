a1a383c450a0de812b0012d596381cac693b9bb3
Kilian Vounckx <r0754877@esat.kuleuven.be>
2021-06-24 00:11:12 +0200

    Update projectile example

--- examples/projectile.rs
+++ examples/projectile.rs
@@ -1,3 +1,5 @@
+use rtc::canvas::Canvas;
+use rtc::color::Color;
 use rtc::tuple::Tuple;
 
 struct Projectile {
@@ -17,10 +19,10 @@ struct Environment {
 	wind: Tuple,
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let mut p = Projectile {
 		position: Tuple::point(0.0, 1.0, 0.0),
-		velocity: Tuple::vector(1.0, 1.0, 0.0).normalized(),
+		velocity: Tuple::vector(1.0, 1.8, 0.0).normalized() * 11.25,
 	};
 
 	let e = Environment {
@@ -28,8 +30,23 @@ fn main() {
 		wind: Tuple::vector(-0.01, 0.0, 0.0),
 	};
 
+	let mut canvas = Canvas::new(900, 550);
+	let color = Color::new(0.1, 0.8, 0.2);
+
 	while p.position.y() > 0.0 {
-		println!("{}", p.position);
 		p.tick(&e);
+
+		let x = p.position.x() as usize;
+		let y = canvas.height() - p.position.y() as usize - 1;
+
+		if x > canvas.width() - 1 || y > canvas.height() - 1 {
+			continue;
+		}
+
+		canvas[[x, y]] = color;
 	}
+
+	let mut stdout = std::io::stdout();
+	canvas.to_ppm(&mut stdout)?;
+	Ok(())
 }
