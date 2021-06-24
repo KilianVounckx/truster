944a3e42afa63fc21237f8ffb81e41c83bc59308
Kilian Vounckx <r0754877@esat.kuleuven.be>
2021-06-24 17:53:48 +0200

    Add light field getters

--- src/light.rs
+++ src/light.rs
@@ -10,7 +10,18 @@ pub struct PointLight {
 }
 
 impl PointLight {
+	/// Creates and returns a new point light at the given position with the given color.
 	pub fn new(position: Tuple, color: Color) -> Self {
 		Self { position, color }
+	}
+
+	/// Returns `self`'s position.
+	pub fn position(&self) -> Tuple {
+		self.position
+	}
+
+	/// Returns `self`'s color.
+	pub fn color(&self) -> Color {
+		self.color
 	}
 }
